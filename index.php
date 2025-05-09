f54744230986e94e3727c02c6bcd707e09a72b71
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-05-09 17:59:15 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NW ChatBot</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="chat-container">
+    	<h2>NW Chatbot</h2>
+    	<div id="chat-box"></div>
+    	<div class="input-container">
+    		<input type="text" id="user-input"  placeholder="Type your message...">
+    		<button onclick="sendMessage()">Send</button>
+    	</div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
